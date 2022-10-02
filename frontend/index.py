3b8d18a87023381fd7a935bab3355c92d9d5b9ec
Vladimir Caterov <vladcaterovdev@gmail.com>
2022-10-01 23:52:39 -0500

    frontend setUp

---
+++ frontend/index.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+increasing = False
+decreasing = False
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
