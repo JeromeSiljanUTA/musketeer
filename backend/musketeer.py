c554ba6dc21b674e6440c7dfc74ad7a19a6fbf4d
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-02 00:00:05 -0500

    pushes data to cockroachdb

--- backend/musketeer.py
+++ backend/musketeer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import psycopg2
 
-#db_url = 'postgresql://vladimir:auCN0jNaEMIhSArPYHdhEg@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dmusketeer-db-6480'
+db_url = 'postgresql://vladimir:auCN0jNaEMIhSArPYHdhEg@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dmusketeer-db-6480'
 
 df = pd.read_csv('tsla.us.txt')
 
@@ -11,15 +11,21 @@ df['datetime'] = [pd.to_datetime(f'{x}{y}', format='%Y%m%d%H%M00')
 df = df[['<TICKER>', 'datetime', '<CLOSE>']]
 df.columns = ['ticker', 'datetime', 'price']
 
-print(df)
+conn = psycopg2.connect(db_url)
 
-# pandas.iterrows
+df_iter = df.iterrows()
 
-#conn = psycopg2.connect(db_url)
+#cur.execute('CREATE TABLE stocks(id UUID NOT NULL DEFAULT gen_random_uuid(), ticker STRING NOT NULL, datetime TIMESTAMP NOT NULL, price FLOAT NOT NULL, UNIQUE(ticker, datetime, price))')
 
-#with conn.cursor() as cur:
-#    cur.execute('SET DATABASE = defaultdb')
-#    cur.execute('SHOW TABLES')
-#    res = cur.fetchall()
-#    conn.commit()
-#    print(res)
+with conn.cursor() as cur:
+    cur.execute('SET DATABASE = defaultdb')
+
+    for row in df_iter:
+        ticker = row[1][0]
+        datetime = row[1][1]
+        price = row[1][2]
+        cur.execute(f"INSERT INTO stocks (ticker, datetime, price) VALUES('{ticker}', '{datetime}', {price})")
+
+    cur.execute('SELECT * FROM stocks')
+    print(cur.fetchall())
+    conn.commit()
