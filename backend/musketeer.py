4232bff462148bd0751d521418cc6998c08d2225
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-01 20:49:14 -0500

    cleans data

--- backend/musketeer.py
+++ backend/musketeer.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import psycopg2
 
-df = pd.read_csv('tsla.us.txt')
+db_url = 'postgresql://vladimir:auCN0jNaEMIhSArPYHdhEg@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dmusketeer-db-6480'
 
-df = df[['<DATE>', '<TIME>', '<CLOSE>']]
-df.columns = ['date', 'time', 'value']
+#df = pd.read_csv('tsla.us.txt')
+#
+#df['datetime'] = [pd.to_datetime(f'{x}{y}', format='%Y%m%d%H%M00') 
+#        for x, y in zip(df['<DATE>'], df['<TIME>'])]
+#
+#df = df[['<TICKER>', 'datetime', '<CLOSE>']]
+#df.columns = ['ticker', 'datetime', 'price']
 
-df['time'] = df['time']/100
+conn = psycopg2.connect(db_url)
 
-df['datetime'] = df['date'] + df['time']
-
-print(df)
+with conn.cursor() as cur:
+    cur.execute('SET DATABASE = defaultdb')
+    cur.execute('SHOW TABLES')
+    res = cur.fetchall()
+    conn.commit()
+    print(res)
