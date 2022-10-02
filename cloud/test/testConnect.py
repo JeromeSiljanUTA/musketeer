eabe9c3bc6a992a24e9428c2041248dabd6987d1
Vladimir Caterov <vladcaterovdev@gmail.com>
2022-10-01 20:44:29 -0500

    Database Setup

--- cloud/test/testConnect.py
+++ cloud/test/testConnect.py
@@ -1,9 +1,14 @@
 import os
 import psycopg2
-DATABASE_STRING = "postgresql://vladimir:auCN0jNaEMIhSArPYHdhEg@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dmusketeer-db-6480"
-with psycopg2.connect(DATABASE_STRING) as conn:
-    with conn.cursor() as cur:
-        cur.execute("SELECT now()")
-        res = cur.fetchall()
-        conn.commit()
-        print(res)
+
+DATABASE_STRING = 'postgresql://vladimir-test:39KNZtj6uqDTJXZFTmA0XA@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dtesting-6482'
+conn = psycopg2.connect(DATABASE_STRING)
+
+with conn.cursor() as cur:
+    
+    cur.execute('CREATE TABLE IF NOT EXISTS TweetTable (id INT PRIMARY KEY, tweetTime TIMESTAMP , tweetText CHAR(280), company CHAR(4));')
+    cur.execute('CREATE TABLE IF NOT EXISTS StockTable (id INT PRIMARY KEY, valueTime TIMESTAMP , value INT, company CHAR(4));')
+    conn.commit()
+    conn.close ()
+
+
